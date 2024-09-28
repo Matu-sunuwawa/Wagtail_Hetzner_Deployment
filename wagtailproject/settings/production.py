82c723175953f9812c4a0946940765a252698a0f
Matu-sunuwawa <matyassinaadugna@gmail.com>
2024-09-28 10:03:43 -0400

    third commit

--- wagtailproject/settings/production.py
+++ wagtailproject/settings/production.py
@@ -2,6 +2,10 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-o*8e$l2h4fvz=f^zi=yac3+oak&xt%!re2sn463c(9#jbk(2ju"
+
+# SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["49.12.41.73"]
 
 try:
